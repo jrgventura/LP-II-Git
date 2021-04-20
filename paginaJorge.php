4be94a50d0c90b760b89ac88b59953bf5bb8a996
Jorge Ventura <jorge.venturag@outlook.com>
2021-04-19 23:26:49 -0500

    esto es nuevo

--- paginaJorge.php
+++ paginaJorge.php
@@ -1,5 +1,7 @@
 <?php 
 
 $echo "Esto es un mensaje en php - desde la pagina de Jorge";
+$echo "Esto es un mensaje en php - desde la pagina de Jorge";
+$echo "Esto es un mensaje en php - desde la pagina de Jorge";
 
 ?>
