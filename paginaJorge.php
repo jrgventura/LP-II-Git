c7214da728f53ddcce41f193db06a91ee2417532
Jorge Ventura <jorge.venturag@outlook.com>
2021-04-19 23:55:02 -0500

    (feature): add new print to file paginaJorge.php

--- paginaJorge.php
+++ paginaJorge.php
@@ -7,4 +7,19 @@ $echo "Esto es un mensaje en php - desde la pagina de Jorge";
 
 $echo "Esto es un mensaje en php - desde la pagina de Jorge";
 
+function foo()
+{
+    $var = 10;
+
+    if ($var === 42) {
+        echo('matched');
+    }
+
+    if ($var === 42) {
+        echo('matched');
+    } else {
+        echo('not matched');
+    }
+}
+
 ?>
